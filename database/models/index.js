529148e6c4d9def994e036a2728e8da9c511861c
Vitor Savedra <vitor-savedra@outlook.com>
2020-05-11 20:16:32 -0300

    :building_construction: Modeling database

--- database/models/index.js
+++ database/models/index.js
@@ -1,17 +1,16 @@
-/* eslint-disable import/no-dynamic-require */
-
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
+const envConfigs = require('../config/config');
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(`${__dirname}/../config/config.js`)[env];
+const config = envConfigs[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (config.url) {
+  sequelize = new Sequelize(config.url, config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
